e3127ad6e631a9bd76fa5c1f20622341a0afaedf
emrecancc <emrecancerli55@gmail.com>
2026-07-03 00:42:50 +0300

    fix: add fallback for undefined user in processUser

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,6 @@
-// src/utils.js
-
-export const transformUser = (x) => ({
-  id: x.id,
-  name: x.name,
-  email: x.email,
-});
+function processUser(user) {
+  const { street } = user || {};
+  // other logic
+  return street;
+}
+module.exports = { processUser };
