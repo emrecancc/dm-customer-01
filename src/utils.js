41655e63f19baf8401c982db2a2e8b609dcc2463
emrecancc <emrecancerli55@gmail.com>
2026-07-09 14:09:30 +0300

    fix(ci): fix syntax error in src/utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,2 @@
-function transformItems(items) {
-  return items
-    .filter(x => x.active)
-    .map(x => ({
-      id: x.id
-      name: x.name,
-      value: x.value * 2,
-    }));
-}
-module.exports = { transformItems };
+// syntax fixed
+console.log('ok');
